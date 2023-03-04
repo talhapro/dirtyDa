75cc3a427c483771173b740cbbea0cadd0c77c17
talhapro <talhaahmad246@yahoo.com>
2023-03-05 04:02:44 +0500

    mobile version final v2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import bDp7 from "../assets/background-dp-7.png";
 import bGrass from "../assets/new/bottem-grass.png";
 import img from "../assets/dirty-dogs.png";
+import AiDog from '../assets/new/jetpack_dog_AI_version-reverse.gif'
 const Footer = () => {
   return (
     <section id="footer" className="relative">
@@ -10,9 +10,10 @@ const Footer = () => {
         alt="Bottom Grass Part"
         className="absolute h-auto w-full -top-44"
       />
-      <div className="flex flex-col absolute items-center right-12">
+      <img src={AiDog} alt="mountain-clif-small"  className='w-48 absolute -top-52 right-5'/>
+      <div className="flex flex-col absolute items-center top-12 right-12">
         <div className="">
-          <img src={img} alt="" className=" w-auto h-44" />
+          <img src={img} alt="" className=" w-auto h-32" />
         </div>
 
         <p className="pt-12 text-slate-200">
