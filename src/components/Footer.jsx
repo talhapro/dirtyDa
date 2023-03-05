b6e2b9620c38c84dd6f72ebf8c0e2dcae1f0fc7e
talhapro <talhaahmad246@yahoo.com>
2023-03-06 01:05:14 +0500

    tablel test v4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import bGrass from "../assets/new/bottem-grass-md.png";
+import bGrassmb from "../assets/new/bottem-grass.png";
 import img from "../assets/dirty-dogs.png";
 import AiDog from '../assets/new/jetpack_dog_AI_version-reverse.gif'
 const Footer = () => {
   return (
     <section id="footer" className="relative flex flex-col items-center justify-center">
       <img
+        src={bGrassmb}
+        alt="Bottom Grass Part"
+        className="absolute h-auto w-full -top-44 sm:hidden"
+      />
+      <img
         src={bGrass}
         alt="Bottom Grass Part"
-        className="absolute h-auto w-full -top-44"
+        className="absolute h-auto w-full -top-44 hidden sm:block"
       />
       <img src={AiDog} alt="mountain-clif-small"  className='w-48 absolute -top-52 right-5 sm:w-56'/>
       <div className="flex flex-col absolute items-center justify-center">
